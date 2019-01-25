9958f75a50c9e6dd5be20feb011530b69126358f
lizardxcc <43263841+lizardxcc@users.noreply.github.com>
2019-01-26 03:44:12 +0900

    Implement Ray.
    Also fix bug caused by missing the intent of the order of for loop which generates pnm file.

--- main.cpp
+++ main.cpp
@@ -162,15 +162,57 @@ inline vec3 unit_vector(vec3 v)
 	return v / v.length();
 }
 
+class ray {
+public:
+	ray()
+	{
+	}
+
+	ray(const vec3& a, const vec3& b)
+	{
+		A = a;
+		B = b;
+	}
+
+	vec3 origin() const { return A; }
+	vec3 direction() const { return B; }
+	vec3 point_at_parameter(float t) const {
+		return A + t * B;
+	}
+
+	vec3 A;
+	vec3 B;
+};
+
+vec3 color(const ray& r)
+{
+	vec3 unit_direction = unit_vector(r.direction());
+	float t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+
+}
+
 int main(void)
 {
 	int nx = 200;
 	int ny = 100;
 
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-	for (int j = 0; j < ny; j++) {
+
+	vec3 lower_left_corner(-2.0, -1.0, -1.0);
+	vec3 horizontal(4.0, 0.0, 0.0);
+	vec3 vertical(0.0, 2.0, 0.0);
+	vec3 origin(0.0, 0.0, 0.0);
+
+
+	for (int j = ny-1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
-			vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+			float u = float(i) / float(nx);
+			float v = float(j) / float(ny);
+			ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+			vec3 col = color(r);
+
+			//vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
 			int ir = int(255.99*col[0]);
 			int ig = int(255.99*col[1]);
 			int ib = int(255.99*col[2]);
