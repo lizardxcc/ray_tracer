685395d55076aa474345bda9efc5389413c1b7f2
lizardxcc <43263841+lizardxcc@users.noreply.github.com>
2019-01-27 23:18:17 +0900

    Implement antialias

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <random>
 #include "vec3.h"
 #include "ray.h"
+#include "camera.h"
 #include "sphere.h"
 #include "hitablelist.h"
 
@@ -28,7 +29,7 @@ vec3 random_in_unit_sphere(void)
 
 vec3 color(const ray& r, hitable *world, int count)
 {
-	if (count >= 100) {
+	if (count >= 20) {
 
 		vec3 unit_direction = unit_vector(r.direction());
 		float t = 0.5 * (unit_direction.y() + 1.0);
@@ -52,27 +53,29 @@ int main(void)
 	int nx = 800;
 	int ny = 400;
 
-	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+	int ns = 100;
 
-	vec3 lower_left_corner(-2.0, -1.0, -1.0);
-	vec3 horizontal(4.0, 0.0, 0.0);
-	vec3 vertical(0.0, 2.0, 0.0);
-	vec3 origin(0.0, 0.0, 0.0);
+	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
 	hitable *list[2];
 	list[0] = new sphere(vec3(0, 0, -1), 0.5);
 	list[1] = new sphere(vec3(0, -100.5, -1), 100);
 	hitable *world = new hitable_list(list, 2);
+	camera cam;
 
 
 	for (int j = ny-1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
-			float u = float(i) / float(nx);
-			float v = float(j) / float(ny);
-			ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-			vec3 col = color(r, world, 0);
-			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+			vec3 col(0, 0, 0);
+			for (int s = 0; s < ns; s++) {
+				float u = float(i + drand48()) / float(nx);
+				float v = float(j + drand48()) / float(ny);
+				ray r = cam.get_ray(u, v);
+				col += color(r, world, 0);
+			}
 
+			col /= float(ns);
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 			//vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
 			int ir = int(255.99*col[0]);
 			int ig = int(255.99*col[1]);
