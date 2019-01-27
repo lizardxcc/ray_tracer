c2737a10c87069cfece2f7f2cb5a7c9b3754859b
lizardxcc <43263841+lizardxcc@users.noreply.github.com>
2019-01-27 17:54:57 +0900

    Implement caluclation of a normal vector

--- main.cpp
+++ main.cpp
@@ -26,27 +26,38 @@ public:
 
 
 
-bool hit_sphere(const vec3& center, float radius, const ray& r)
+float hit_sphere(const vec3& center, float radius, const ray& r)
 {
 	float a = pow(r.B.length(), 2.0);
 	float b_prime = dot(r.A - center, r.B);
 	float c = pow((r.A - center).length(), 2.0) - pow(radius, 2.0);
 	float discriminant_prime = pow(b_prime, 2.0) - a * c;
-	if (discriminant_prime >= 0.0) {
-		return true;
+	if (discriminant_prime < 0.0) {
+		return -1.0;
 	}
-	return false;
+	float t1 = (-b_prime - sqrt(discriminant_prime)) / a;
+	float t2 = (-b_prime + sqrt(discriminant_prime)) / a;
+	if (t1 >= 0.0 && t2 >= 0.0) {
+		return std::min(t1, t2);
+	} else {
+		return std::max(t1, t2);
+	}
 }
 
 
 vec3 color(const ray& r)
 {
-	if (hit_sphere(vec3(0, 0, -1), 0.5, r))
-		return vec3(1, 0, 0);
+	vec3 center = vec3(0, 0, -1);
+	float t = hit_sphere(center, 0.5, r);
+	if (t >= 0.0) {
+		vec3 hit_point = r.point_at_parameter(t);
+		vec3 normal = unit_vector(hit_point - center);
+		return 0.5*vec3(normal.x()+1, normal.y()+1, normal.z()+1);
+	}
 
 	vec3 unit_direction = unit_vector(r.direction());
-	float t = 0.5 * (unit_direction.y() + 1.0);
-	return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+	float s = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0-s)*vec3(1.0, 1.0, 1.0) + s*vec3(0.5, 0.7, 1.0);
 }
 
 int main(void)
